a817d1ce6dcd025bb12010d64014ac3d2ac5bcaf
Suphaloek Khueanphet <suphaloek_khueanphet@cmu.ac.th>
2023-12-29 00:10:22 +0700

    complete_all

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <class T>
+void mySwap(T&x,T&y);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +33,9 @@ int main(){
 	return 0;
 }
 
+template <class T>
+void mySwap(T&x,T&y){
+    T a=x;
+    x=y ;
+    y=a ;
+}
